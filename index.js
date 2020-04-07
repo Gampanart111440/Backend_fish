ec0eab9b8c71dbc9d9d79f1abc99074d6cb435e4
Gampanart Puangkaew <s5935512089@phuket.psu.ac.th>
2020-04-07 20:30:09 +0700

    add like

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ let data_fishs = [
         'common_name': 'ปลาสลาด',
         'scientific_name': 'Notopterus notopterus',
         'image': 'https://4.bp.blogspot.com/-B5lNXtPJ288/TfCR7x8GuiI/AAAAAAAAAwo/5GAPVYW0gLY/s400/Notopterus+notopterus+%2528FOLI%2529.jpg',
-        'fish_detail': 'ปลาสลาด หรือ ปลาฉลาด เป็นปลาน้ำจืดชนิดหนึ่ง มีชื่อวิทยาศาสตร์ว่า Notopterus notopterus อยู่ในวงศ์ปลากราย มีปากกว้างไม่เกินลูกตาเหมือนปลาในวงศ์นี้ชนิดอื่น ๆ พื้นลำตัวมีสีเรียบ ยกเว้นปลาวัยอ่อนจะมีลายบั้งเหมือนปลากราย วัยอ่อน จมูกมีสองคู่ คู่หน้ายื่นออกมาคล้ายหลอดหรือหนวด'
+        'fish_detail': 'ปลาสลาด หรือ ปลาฉลาด เป็นปลาน้ำจืดชนิดหนึ่ง มีชื่อวิทยาศาสตร์ว่า Notopterus notopterus อยู่ในวงศ์ปลากราย มีปากกว้างไม่เกินลูกตาเหมือนปลาในวงศ์นี้ชนิดอื่น ๆ พื้นลำตัวมีสีเรียบ ยกเว้นปลาวัยอ่อนจะมีลายบั้งเหมือนปลากราย วัยอ่อน จมูกมีสองคู่ คู่หน้ายื่นออกมาคล้ายหลอดหรือหนวด',
+        'like': 0
     },
     {
         'id': 0,
@@ -26,12 +27,12 @@ let data_fishs = [
         'common_name': 'ปลาน้ำหมึก',
         'scientific_name': 'Opsarius sp.',
         'image': 'https://upload.wikimedia.org/wikipedia/commons/f/fb/OP2.jpg',
-        'fish_detail': 'ปลาน้ำหมึก เป็นปลาน้ำจืดชนิดหนึ่ง มีชื่อวิทยาศาสตร์ว่า Opsarius pulchellus อยู่ในวงศ์ปลาตะเพียน รูปร่างคล้ายปลาน้ำหมึกโคราช ซึ่งเป็นปลาในสกุลเดียวกัน ต่างกันที่น้ำหมึกมีลำตัวที่ป้อมสั้นกว่า ปลายปากป้าน มีสีสันที่สดใสกว่าและลายขีดข้างลำตัวใหญ่และชัดเจนกว่า เกล็ดมีขนาดใหญ่กว่า ครีบหลังมีแต้มสีแดงเห็นชัดเจน'
+        'fish_detail': 'ปลาน้ำหมึก เป็นปลาน้ำจืดชนิดหนึ่ง มีชื่อวิทยาศาสตร์ว่า Opsarius pulchellus อยู่ในวงศ์ปลาตะเพียน รูปร่างคล้ายปลาน้ำหมึกโคราช ซึ่งเป็นปลาในสกุลเดียวกัน ต่างกันที่น้ำหมึกมีลำตัวที่ป้อมสั้นกว่า ปลายปากป้าน มีสีสันที่สดใสกว่าและลายขีดข้างลำตัวใหญ่และชัดเจนกว่า เกล็ดมีขนาดใหญ่กว่า ครีบหลังมีแต้มสีแดงเห็นชัดเจน',
+        'like': 0
     },
 ]
 
 app.post('/login', (req, res) => {
-    console.log(req.body);
     soap.createClient(url, (err, client) => {
         if (err) console.error(err);
         else {
@@ -41,7 +42,6 @@ app.post('/login', (req, res) => {
             client.GetStudentDetails(user, function (err, response) {
                 if (err) console.error(err);
                 else {
-                    console.log(response);
                     res.send(response);
                 }
             });
@@ -56,12 +56,10 @@ app.get("/datafish", (req, res) => {
 app.get('/datafish/:id', (req, res) => {
     let id = req.params.id
     let data_fish = data_fishs.find(p => (p.id === +id))
-    console.log(data_fish);
     res.json(data_fish);
 })
 
 app.post('/fish', (req, res) => {
-    console.log(req.body);
     var data_fish = {}
     data_fish.id = data_fishs.length > 0 ? data_fishs[0].id + 1 : 0
     data_fish.local_name = req.body.local_name
@@ -69,12 +67,12 @@ app.post('/fish', (req, res) => {
     data_fish.scientific_name = req.body.scientific_name
     data_fish.image = req.body.image
     data_fish.fish_detail = req.body.fish_detail
+    data_fish.like = req.body.like
     data_fishs.unshift(data_fish)
     res.json({ message: "Successfully" })
 })
 
 app.put('/update/:id_fish', (req, res) => {
-    console.log(req.body);
     let id = req.params.id_fish
     let idx = data_fishs.findIndex(p => (p.id === +id))
     data_fishs[idx].local_name = req.body.local_name
@@ -82,6 +80,7 @@ app.put('/update/:id_fish', (req, res) => {
     data_fishs[idx].scientific_name = req.body.scientific_name
     data_fishs[idx].image = req.body.image
     data_fishs[idx].fish_detail = req.body.fish_detail
+    data_fishs[idx].like = req.body.like
     res.json({ message: 'Data fish updated! id: ' + req.params.id_fish });
 })
 
